900765d2aa7a1ba81e2fd7f9e247884ca395f6f8
Rajdeep Dey <rajdeep.dey.fiem.bca23@teamfuture.in>
2026-04-10 16:16:15 +0530

    Remove unnecessary blank lines in plan_screen.dart

--- lib/screens/payment/plan_screen.dart
+++ lib/screens/payment/plan_screen.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-
 import '../../cards/payment/plan_card.dart';
 import '../../models/plans.dart';
 import '../../notifiers/payment_notifier.dart';
